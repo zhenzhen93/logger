4d3cd06fdb20d022792109dba691ae0085ca2263
zhenzhen.xie <zhenzhen.xie@zhangmen.com>
2020-04-16 17:43:43 +0800

    添加一个效果图

--- sample/src/main/java/com/orhanobut/sample/App.kt
+++ sample/src/main/java/com/orhanobut/sample/App.kt
@@ -8,7 +8,11 @@ class App : Application() {
   override fun onCreate() {
     super.onCreate()
     //日志初始化
-    LogX.register(this)
+    LogX.register(this, true, object : TrackCallback {
+      override fun onEventImme(eventId: String, eventParam: Map<String, String>?) {
+        LogX.errorZhen("全链路日志埋点： $eventId  $eventParam")
+      }
+    })
 
   }
 
